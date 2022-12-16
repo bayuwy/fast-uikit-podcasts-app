7fb9af3ef1ccde8572f7a52e3497a3d322b10faa
Bayu Yasaputro <bayuwy@gmail.com>
2022-12-16 22:19:42 +0700

    Initial Commit

---
+++ PodcastsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodcastsApp
+//
+//  Created by Bayu Yasaputro on 16/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
